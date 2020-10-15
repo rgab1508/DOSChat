f9ba44f45b6f4516517b3efa4d7af7707c83ebd0
Gabriel Rajendran <rgabriel1508@gmail.com>
2020-10-15 20:42:12 +0530

    added files

---
+++ Client.java
@@ -0,0 +1,10 @@
+import java.net.Socket;
+
+
+class Client {
+
+	public static void main(String args[]){
+		System.out.println("Client clienting");
+	}
+
+}
---
+++ Server.java
@@ -0,0 +1,10 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+class Server {
+	
+	public static void main(String args[]){
+		System.out.println("Server serving.......");
+	}
+}
