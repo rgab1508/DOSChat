81055a55ce27fb33651a4d8e6ab35654146506fd
Gabriel Rajendran <rgabriel1508@gmail.com>
2020-10-16 19:21:22 +0530

    working on Server & Client

--- Client.java
+++ Client.java
@@ -1,10 +1,36 @@
 import java.net.Socket;
-
+import java.io.*;
 
 class Client {
+	private static String ADDR = "127.0.0.1";
+	private static int PORT;
+	private static String name;
 
-	public static void main(String args[]){
-		System.out.println("Client clienting");
+	public static void main(String args[]) throws IOException {
+		BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("+---------DOSChat--------+");
+		System.out.print("Enter the PORT : ");
+		PORT = Integer.parseInt(keyboard.readLine());
+		System.out.println("port is " + PORT);
+
+		Socket s = new Socket(ADDR, PORT);
+		System.out.println("Connected to server..");
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+		PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+
+		System.out.println("Username : ");
+		name = keyboard.readLine();
+
+		out.println(name);
+
+		while(true){
+			System.out.print(name + "> ");
+			String req = keyboard.readLine();
+			out.println(req);
+			String res = in.readLine();
+			System.out.println(res);
+		}
 	}
 
 }
--- Server.java
+++ Server.java
@@ -1,10 +1,83 @@
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+/*
+class ServerConnection implements Runnable{
+	Socket s;
+	BufferedReader in;
+	public ServerConnection(Socket s){
+		this.s = s;
 
+		in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+	}
+
+	public void run(){
+		String msg = in.readLine();
+		System.out.println(msg);
+	}
+
+}
+*/
+class ClientHandler implements Runnable {
+	public String name;
+	public Socket s;
+	public BufferedReader in;
+	public PrintWriter out;
+	public ArrayList<ClientHandler> clients = new ArrayList<>();
+
+	public ClientHandler(Socket s, String name, ArrayList<ClientHandler> clients) throws IOException{
+		this.name =  name;
+		this.s = s;
+		this.clients = clients;
+
+			in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+			out = new PrintWriter(s.getOutputStream(), true);
+	}
+
+	public void run(){
+		try {
+			while(true){
+			
+				String msg = in.readLine();
+				sendToAll(this.name +"> " + msg);
+			}
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+
+	public void sendToAll(String msg){
+		for(ClientHandler client: this.clients){
+			//if(client.equals(this.s)){
+				client.out.println(msg);
+			//}
+		}
+	}
+}
 
 class Server {
+	private static int PORT = 9001;
+	private static ArrayList<ClientHandler> clients = new ArrayList<>();
+	private static ExecutorService pool = Executors.newFixedThreadPool(10);
 	
-	public static void main(String args[]){
+	public static void main(String args[]) throws IOException {
 		System.out.println("Server serving.......");
+	
+		ServerSocket ss = new ServerSocket(PORT);
+	
+		// s is Client Socket
+		while (true){
+			Socket s = ss.accept();
+			BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));	
+			String name = in.readLine();
+			ClientHandler client = new ClientHandler(s, name, clients);
+			clients.add(client);
+			System.out.println(name + " has connected.");
+
+			pool.execute(client);
+		}
 	}
 }
